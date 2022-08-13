b889d3a7d9ed9a5333364048731c6369c6a10449
kkrishna-choudhary <65896774+kkrishna-choudhary@users.noreply.github.com>
2022-08-13 13:57:48 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/padhaiGhad',
-    'google': {
-        GOOGLE_CLIENT_ID: "550187692132-g727iefdbbok7ccuvmvq65fpm98u8uvc.apps.googleusercontent.com",
-        GOOGLE_CLIENT_SECRET: "TNbA2nw_j5fNKrTR8WRfvyEq"
-    }
-}
